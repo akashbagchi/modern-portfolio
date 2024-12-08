4f8f5a7fc674cb04ff58fbc731b072bc4506705e
Akash Bagchi <72804789+akashbagchi@users.noreply.github.com>
2024-12-08 16:00:00 +0900

    fix 🐛 eslint config format

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,28 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  files: ['**/*.{js,jsx,ts,tsx,vue,json}'],
+    vue: true,
+    typescript: true,
 
-  ignores: [
-    '**/node_modules/**',
-    '**/dist/**',
-    '.nuxt',
-    '.output',
-    '*.log',
-    '.env',
-  ],
+},
+    {
+        files: ['**/*.{js,jsx,ts,tsx,vue,json}'],
+        ignores: [
+            '**/node_modules/**',
+            '**/dist/**',
+            '.nuxt',
+            '.output',
+            '*.log',
+            '.env',
+        ],
 
-  vue: true,
-  typescript: true,
-
-  rules: {
-    'vue/multi-word-component-names': 'off',
-    'vue/no-v-html': 'off',
-    'vue/require-default-prop': 'off',
-    'no-unused-vars': ['warn', {
-      argsIgnorePattern: '^_',
-      varsIgnorePattern: '^_'
-    }],
-  }
-})
+        rules: {
+            'vue/multi-word-component-names': 'off',
+            'vue/no-v-html': 'off',
+            'vue/require-default-prop': 'off',
+            'no-unused-vars': ['warn', {
+                argsIgnorePattern: '^_',
+                varsIgnorePattern: '^_'
+            }],
+        }
+    })
